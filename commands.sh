227d067e085d652eda8d98dc2fcc4604aba84da3
Your Name <you@example.com>
2023-09-20 23:05:58 +0000

    m install easier

--- commands.sh
+++ commands.sh
@@ -1,5 +1,7 @@
 ssh-keygen -t ed25519 -C "your_email@example.com"
+git clone git@github.com:Lewington-pitsos/spotify-albums.git
 pip install virtualenv
 virtualenv venv 
 source venv/bin/activate
 pip install -r requirements.txt
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
